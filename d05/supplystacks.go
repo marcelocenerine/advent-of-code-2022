c51a419b80b79e29b453d961f1b0a8e3680080b4
Marcelo Cenerino <marcelocenerine@gmail.com>
2022-12-24 14:05:44 +0000

    completes the second part of day #5

--- d05/supplystacks.go
+++ d05/supplystacks.go
@@ -12,23 +12,25 @@ import (
 )
 
 func main() {
-	stacks, arrangements, err := parseInputFile("input.txt")
-	if err != nil {
-		fmt.Printf("Invalid input: %v\n", err)
-		os.Exit(1)
+	for _, crane := range []crane{cm9000{}, cm9001{}} {
+		stacks, arrangements, err := parseInputFile("input.txt")
+		if err != nil {
+			fmt.Printf("Invalid input: %v\n", err)
+			os.Exit(1)
+		}
+		rearranged, err := rearrange(stacks, arrangements, crane)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Result: ", topCrates(rearranged))
 	}
-	err = rearrange(stacks, arrangements)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Printf("Cranes at the top of the stacks: %s\n", part1CranesOnTop(stacks))
 }
 
-func part1CranesOnTop(stacks []*stack) string {
+func topCrates(stacks []*stack) string {
 	var buffer bytes.Buffer
-	for _, stk := range stacks {
-		if top, err := stk.peek(); err == nil {
+	for _, s := range stacks {
+		if top, err := s.peek(); err == nil {
 			buffer.WriteRune(rune(top))
 		}
 	}
@@ -38,7 +40,36 @@ func part1CranesOnTop(stacks []*stack) string {
 type crate rune
 
 func (c crate) String() string {
-	return fmt.Sprintf("[%s]", string(c))
+	return string(c)
+}
+
+type crane interface {
+	move(n int, from, to *stack) error
+}
+
+type cm9000 struct{}
+
+func (cm cm9000) move(n int, from, to *stack) error {
+	if from.size() < n {
+		return fmt.Errorf("from.size < n: size=%d, n=%d", from.size(), n)
+	}
+	for i := 0; i < n; i++ {
+		top, _ := from.pop()
+		to.push(top)
+	}
+	return nil
+}
+
+type cm9001 struct{}
+
+func (cm cm9001) move(n int, from, to *stack) error {
+	if from.size() < n {
+		return fmt.Errorf("from.size < n: size=%d, n=%d", from.size(), n)
+	}
+	crates := from.crates[from.size()-n:]
+	to.crates = append(to.crates, crates...)
+	from.crates = from.crates[:from.size()-n]
+	return nil
 }
 
 type stack struct {
@@ -50,6 +81,13 @@ func (s *stack) push(c crate) {
 	s.crates = append(s.crates, c)
 }
 
+func (s *stack) peek() (crate, error) {
+	if s.size() == 0 {
+		return 0, fmt.Errorf("stack %d is empty", s.id)
+	}
+	return s.crates[s.size()-1], nil
+}
+
 func (s *stack) pop() (crate, error) {
 	top, err := s.peek()
 	if err != nil {
@@ -59,19 +97,12 @@ func (s *stack) pop() (crate, error) {
 	return top, nil
 }
 
-func (s *stack) peek() (crate, error) {
-	if s.size() == 0 {
-		return 0, fmt.Errorf("stack %d is empty", s.id)
-	}
-	return s.crates[s.size()-1], nil
-}
-
 func (s *stack) size() int {
 	return len(s.crates)
 }
 
 func (c *stack) String() string {
-	return fmt.Sprintf("{id=%d, crates=%v}", c.id, c.crates)
+	return fmt.Sprintf("%d - %v", c.id, c.crates)
 }
 
 type step struct {
@@ -141,9 +172,9 @@ func parseStacks(lines []string) ([]*stack, error) {
 }
 
 func parseArrangement(lines []string, rgx *regexp.Regexp) ([]step, error) {
-	result := make([]step, 0, len(lines))
+	result := make([]step, len(lines))
 
-	for _, line := range lines {
+	for i, line := range lines {
 		if !rgx.MatchString(line) {
 			return result, fmt.Errorf("invalid step line: %s", line)
 		}
@@ -151,28 +182,25 @@ func parseArrangement(lines []string, rgx *regexp.Regexp) ([]step, error) {
 		n, _ := strconv.Atoi(groups[0][1])
 		from, _ := strconv.Atoi(groups[0][2])
 		to, _ := strconv.Atoi(groups[0][3])
-		result = append(result, step{n: n, from: from, to: to})
+		result[i] = step{n: n, from: from, to: to}
 	}
 	return result, nil
 }
 
-func rearrange(stacks []*stack, arrangement []step) error {
+func rearrange(stacks []*stack, arrangement []step, cm crane) ([]*stack, error) {
 	for _, step := range arrangement {
 		if step.from > len(stacks) {
-			return fmt.Errorf("invalid 'from' in step: %v", step)
+			return nil, fmt.Errorf("invalid 'from' in step: %v", step)
 		}
 		if step.to > len(stacks) {
-			return fmt.Errorf("invalid 'to' in step: %v", step)
+			return nil, fmt.Errorf("invalid 'to' in step: %v", step)
 		}
 		from := stacks[step.from-1]
 		to := stacks[step.to-1]
-		if step.n > from.size() {
-			return fmt.Errorf("invalid 'n' in step: %v", step)
-		}
-		for i := 0; i < step.n; i++ {
-			top, _ := from.pop()
-			to.push(top)
+		err := cm.move(step.n, from, to)
+		if err != nil {
+			return nil, err
 		}
 	}
-	return nil
+	return stacks, nil
 }
