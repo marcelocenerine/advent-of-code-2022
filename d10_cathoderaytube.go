f6b713ed865ca55d34c7dd1baf999b16b66a4ba7
Marcelo Cenerino <marcelocenerine@gmail.com>
2023-01-03 23:25:38 +0000

    completes the second part of day #10

--- d10_cathoderaytube.go
+++ d10_cathoderaytube.go
@@ -1,6 +1,13 @@
 // Solution to https://adventofcode.com/2022/day/10
 package adventofcode
 
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 type CathodeRayTube struct{}
 
 func (p CathodeRayTube) Details() Details {
@@ -8,8 +15,77 @@ func (p CathodeRayTube) Details() Details {
 }
 
 func (p CathodeRayTube) Solve(input *Input) (Result, error) {
+	instructions, err := p.parseInstructions(input)
+	if err != nil {
+		return Result{}, err
+	}
+	part1 := p.sumOfSignalStrengths(instructions, 20, 40)
+	part2 := p.crtDraw(instructions, 40, 3)
+	if err != nil {
+		return Result{}, err
+	}
 	return Result{
-		Part1: "?",
-		Part2: "?",
+		Part1: strconv.Itoa(part1),
+		Part2: part2,
 	}, nil
+}
+
+func (p CathodeRayTube) sumOfSignalStrengths(instructions []Add, firstCheckpoint, checkpointIntervals int) int {
+	checkpoint := firstCheckpoint
+	register := 1
+	result := 0
+	for cycle := 1; cycle <= len(instructions); cycle++ {
+		instr := instructions[cycle-1]
+		if cycle == checkpoint {
+			result += cycle * register
+			checkpoint += checkpointIntervals
+		}
+		register += instr.Value
+	}
+	return result
+}
+
+func (p CathodeRayTube) crtDraw(instructions []Add, screenWidth, spriteWidth int) string {
+	register := 1
+	var buffer bytes.Buffer
+
+	for cycle, instr := range instructions {
+		pixel := cycle % screenWidth
+		spritePos := register - int(spriteWidth/2)
+
+		if pixel >= spritePos && pixel < spritePos+spriteWidth {
+			buffer.WriteRune('#')
+		} else {
+			buffer.WriteRune('.')
+		}
+		if pixel == screenWidth-1 && cycle < len(instructions)-1 {
+			buffer.WriteRune('\n')
+		}
+		register += instr.Value
+	}
+	return buffer.String()
+}
+
+type Add struct {
+	Value int
+}
+
+func (p CathodeRayTube) parseInstructions(input *Input) ([]Add, error) {
+	noopRgx := regexp.MustCompile(`^noop$`)
+	addxRgx := regexp.MustCompile(`^addx (-?\d+)$`)
+	var result []Add
+
+	for _, line := range input.Lines() {
+		result = append(result, Add{0})
+		switch {
+		case noopRgx.MatchString(line):
+		case addxRgx.MatchString(line):
+			groups := addxRgx.FindAllStringSubmatch(line, -1)
+			value, _ := strconv.Atoi(groups[0][1])
+			result = append(result, Add{value})
+		default:
+			return nil, fmt.Errorf("invalid line: %s", line)
+		}
+	}
+	return result, nil
 }
